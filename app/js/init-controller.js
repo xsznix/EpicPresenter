93fc8b0e4c3b6a3f95ad7410dd290a7ab1324956
Xuming Zeng <xsznix@gmail.com>
2015-07-12 16:57:25 -0700

    open to full screen if two or more screens present

--- app/js/init-controller.js
+++ app/js/init-controller.js
@@ -9,11 +9,30 @@ gui.Screen.Init();
 // - file:open (). Reset all program state.
 global.events = _.clone(Backbone.Events);
 
-// Open presentation
-var presentation = gui.Window.open('presentation.html', {
-	toolbar: false,
-	x: 40,
-	y: 40,
-	height: 480,
-	width: 640
+// Position presentation on second monitor and enter full screen if there are
+// two monitors
+$(function () {
+	var presentationOpts = {
+		toolbar: false,
+		x: 40,
+		y: 40,
+		height: 480,
+		width: 640
+	};
+	var fullScreen = gui.Screen.screens.length > 1;
+
+	if (fullScreen) {
+		_.extend(presentationOpts, {
+			x: gui.Screen.screens[1].work_area.x,
+			y: gui.Screen.screens[1].work_area.y
+		});
+	}
+
+	// Open presentation
+	window.presentation = gui.Window.open('presentation.html', presentationOpts);
+
+	if (fullScreen) {
+		presentation.enterFullscreen();
+		gui.Window.get().enterFullscreen();
+	}
 });
